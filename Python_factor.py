9df44862dab39b62b5947d6673cacbfd7b404be7
Gyanendra Verma <gyanendraverma21@gmail.com>
2015-11-16 20:02:52 +0530

    Assignment 1 submission by katchu

---
+++ Python_factor.py
@@ -0,0 +1,21 @@
+num = int(input("Enter a number:  "))
+x = int
+
+if num < 0:
+    print ("Sorry factors don't exist for negative numbers :(")
+elif num == 0:
+    print ( "The factor of 0 is 0")
+else:
+    for i in range(1, num):
+        if num / i == float:
+            continue
+            i = i-1
+            if i == 0:
+                break
+        if num / i == int:
+            print "The factor(s) are x "
+            continue
+            i = i -1
+        if i == 0:
+            break
+        
---
+++ python_armstrong.py
@@ -0,0 +1,12 @@
+num = int(input("Please enter a number: ")
+num = 100x + 10y + z
+
+if num < 100:
+    print ("Sorry we won't do any numbers below 100 ")
+elif num > 1000:
+    print ("Sorry no more numbers that we can process")
+else num >100 && num < 1000:
+    if num == x*x*x + y*y*y + z*z*z
+          print num
+    else:
+        print ("This isn't an armstrong number")
